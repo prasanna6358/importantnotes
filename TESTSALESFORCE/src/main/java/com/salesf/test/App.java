06121de64e6de64a5eaa5b9c134a3bacca707b99
Billa Prasanna Kumar <prasanna6358@gmail.com>
2017-04-12 18:48:36 +0530

    Update App.java

--- TESTSALESFORCE/src/main/java/com/salesf/test/App.java
+++ TESTSALESFORCE/src/main/java/com/salesf/test/App.java
@@ -5,6 +5,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "Hello World!" );
         LoginSalesForce lsf = new LoginSalesForce();
         System.out.println(lsf.loginSalesForce());
         //LoginSalesForce.getProperties();
